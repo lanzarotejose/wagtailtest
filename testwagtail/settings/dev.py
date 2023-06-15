65999a73cd54f51c1eb57b8bfb0ffe4e127f9715
lanzarotejose <lanzarote.jose@gmail.com>
2023-06-15 22:36:09 +0100

    subir el directorio de testwagtail por primiera vez

---
+++ testwagtail/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-73u5k!l-wm4$=t_=@urruw29w3!s7ro(4kh&_$@x34@bo*kfyb"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
