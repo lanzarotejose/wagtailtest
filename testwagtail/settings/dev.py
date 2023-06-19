748c3ccade2eee8f5e73086a6a74ec4d71a2fb1a
lanzarotejose <lanzarote.jose@gmail.com>
2023-06-19 20:22:13 +0100

    Configuración inicial de Wagtail test

---
+++ testwagtail/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-5c@1d8)u6s2)$27-h8yi-1o#+5ls@k!xrxki-o^-c1_6)=b%44"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
